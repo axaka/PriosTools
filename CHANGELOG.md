53d02fdef8c8c9e666a38244dc7205416f1488df
Martin Vadseth Høiby <martinvadseth@gmail.com>
2025-04-29 14:45:59 +0200

    Added Slider support for PriosEventReciever

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## [1.0.1] - 2024-04-29
+### Slider support
+- Added Slider support for PriosEventReciever
+
 ## [1.0.0] - 2024-04-29
 ### First Release
 - Initial release of the project with basic functionality.
--- Runtime/PriosEventReciever.cs
+++ Runtime/PriosEventReciever.cs
@@ -14,6 +14,7 @@ namespace PriosTools
         public PriosUIAnimator UIAnimator { get; private set; }
         public TMP_Text Text { get; private set; }
         public Toggle Toggle { get; private set; }
+        public Slider Slider{ get; private set; }
         public TMP_InputField InputField { get; private set; }
 
         [Flags]
@@ -27,6 +28,7 @@ namespace PriosTools
             Text = 1 << 4,
             InputField = 1 << 5,
             Toggle = 1 << 6,
+            Slider = 1 << 7,
         }
 
         void Awake()
@@ -37,6 +39,7 @@ namespace PriosTools
             Text = GetComponent<TMP_Text>();
             InputField = GetComponent<TMP_InputField>();
             Toggle = GetComponent<Toggle>();
+            Slider = GetComponent<Slider>();
 
             PriosEvent.AddListener<object>(EventKey, OnEventTriggered);
         }
@@ -116,6 +119,17 @@ namespace PriosTools
                     UnityEngine.Object => !Toggle.isOn, // Toggle if null
                     _ => Toggle.isOn // Default for anything else
                 };
+            }
+
+            // Update Slider if flag is set
+            if ((SelectedComponentTypes & ComponentTypes.Slider) == ComponentTypes.Slider && Slider != null)
+            {
+				Slider.value = obj switch
+				{
+					float f => f,
+                    int i => i,
+					_ => Slider.value,
+				};
             }
         }
     }
